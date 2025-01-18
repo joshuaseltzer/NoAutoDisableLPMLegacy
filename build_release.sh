f3e699d37c20a4e6932a73c46844d6b66485a721
Joshua Seltzer <joshua.seltzer90@gmail.com>
2025-01-17 21:54:49 -0500

    Adding build script. Update control file to indicate iOS 14 was tested

---
+++ build_release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# use the Xcode 11 toolchain to ensure arm64e compatibility on iOS 12 and iOS 13
+export PREFIX=$THEOS/toolchain/Xcode11.xctoolchain/usr/bin/
+
+make clean
+make package FINALPACKAGE=1
