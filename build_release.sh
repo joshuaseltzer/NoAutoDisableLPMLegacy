166fa2f3d3fe4aac70a0e80ed22cfbc7b0380e85
Joshua Seltzer <joshua.seltzer90@gmail.com>
2025-02-17 06:27:59 -0500

    Kill the correct process in postrm

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# immediately exit if any errors occur
+set -e
+
 # use the Xcode 11 toolchain to ensure arm64e compatibility on iOS 12 and iOS 13
 export PREFIX=$THEOS/toolchain/Xcode11.xctoolchain/usr/bin/
 
